84ed10aaa74f198d0d9c96c355820a5562a65c38
Frédéric Dénommé <frederic.denomme@libeo.com>
2021-11-15 08:33:12 -0500

    MIGRATION + STRINGS

--- VOTVOT/models.py
+++ VOTVOT/models.py
@@ -6,7 +6,13 @@ class Question(models.Model):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='owner', related_name='questions')
     text = models.TextField(verbose_name='text')
 
+    def __str__(self):
+        return self.text
+        
 
 class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE, verbose_name='question', related_name='answers')
     text = models.TextField(verbose_name='text')
+
+    def __str__(self):
+        return self.question.text + ' - ' + self.text
